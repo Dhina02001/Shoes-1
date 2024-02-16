00811fa8d564a0c283071e0a73022bc4e3847d83
Dhina02001 <159763094+Dhina02001@users.noreply.github.com>
2024-02-16 17:18:44 +0530

    Add files via upload

---
+++ precache-manifest.3875517450f2400f06dd777e7d3d1dd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ab01816844fc2514eb4a2ab180d54d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16c26de9f98dd15ab511",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "2b3d217399eb476326c9",
+    "url": "/static/css/main.c06bd0b2.chunk.css"
+  },
+  {
+    "revision": "16c26de9f98dd15ab511",
+    "url": "/static/js/2.83728723.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "2b3d217399eb476326c9",
+    "url": "/static/js/main.5a28e53e.chunk.js"
+  },
+  {
+    "revision": "ba90eaf63a0956376345",
+    "url": "/static/js/runtime~main.6f7dce58.js"
+  }
+]);
